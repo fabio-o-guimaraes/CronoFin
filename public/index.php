fc70ef94d9ca281ab11ddc3fe99dfe08a425d3a7
Fábio <fog1991@gmail.com>
2026-09-09 08:06:25 +0100

    developing index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,31 @@
-<?php require_once '../includes/header.php';
+<!DOCTYPE html>
+<html lang="pt-PT">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CronoFin - Gestão de Finanças Pessoais</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/base.css">
+    <link rel="stylesheet" href="assets/css/layout.css">
+    <link rel="stylesheet" href="assets/css/components.css">
+    <!-- <link rel="stylesheet" href="assets/css/pages/profile.css"> editar caso use css especifico em certas páginas-->
+</head>
+
+<body>
+    <?php require_once '../includes/header.php'; ?>
+
+    <main class="home-main">
+        <section class="hero">
+            <h1>Controla as tuas finanças com o CronoFin</h1>
+            <p>Regista movimentos, cria categorias e alcança os teus objetivos de poupança.</p>
+            <img class="hero-img" src="assets/img/moedas.jpg"> <!-- alterar para print do dashboard -->
+        </section>
+    </main>
+
+    <?php require_once '../includes/footer.php'; ?>
+</body>
+
+
+</html>
