e83b221c37936c4b20bb13c736a13a4f99a27fbf
Fábio <fog1991@gmail.com>
2026-09-09 07:27:37 +0100

    header.php completed

--- includes/header.php
+++ includes/header.php
@@ -25,9 +25,19 @@ if ($isLoggedIn) {
             <img src="assets/img/CronoFin_v1.svg" class="brand-name">
         </a>
 
+        <button type="button" class="menu-toggle" aria-label="Abrir menu" aria-expanded="false" aria-controls="main-menu">
+            <span class="menu-toggle-bar" aria-hidden="true"></span>
+            <span class="menu-toggle-bar" aria-hidden="true"></span>
+            <span class="menu-toggle-bar" aria-hidden="true"></span>
+        </button>
 
-
-
+        <ul class="main-menu" id="main-menu">
+            <?php if ($isLoggedIn): ?>
+                <li><a href="logout.php">Sair</a></li>
+            <?php else: ?>
+                <li><a href="login.php">Iniciar Sessão</a></li>
+                <li><a href="register.php">Registo</a></li>
+            <?php endif; ?>
+        </ul>
     </nav>
-
 </header>
