c344210e6026fea0a5b5c71fd561c62453fee45c
Fábio <fog1991@gmail.com>
2026-09-13 09:09:01 +0100

    added logo v2 on header.php + added layout.css

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,7 @@ if ($isLoggedIn) {
 <header class="site-header">
     <nav class="main-nav">
         <a href="<?= $logoLink ?>" class="brand">
-            <img src="assets/img/icon_v1.svg" alt="Logótipo CronoFin" class="brand-logo">
+            <img src="assets/img/icon_v2.svg" alt="Logótipo CronoFin" class="brand-logo">
             <img src="assets/img/CronoFin_v1.svg" class="brand-name">
         </a>
 
@@ -33,10 +33,10 @@ if ($isLoggedIn) {
 
         <ul class="main-menu" id="main-menu">
             <?php if ($isLoggedIn): ?>
-                <li><a href="logout.php">Sair</a></li>
+                <li><a href="logout.php" class="link-on-dark">Sair</a></li>
             <?php else: ?>
-                <li><a href="login.php">Iniciar Sessão</a></li>
-                <li><a href="register.php">Registo</a></li>
+                <li><a href="login.php" class="link-on-dark">Iniciar Sessão</a></li>
+                <li><a href="register.php" class="link-on-dark">Registo</a></li>
             <?php endif; ?>
         </ul>
     </nav>
